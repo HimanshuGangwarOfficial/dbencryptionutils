a49351a03070ce7b83c9cefe6b17755ae297c05a
Himanshu Gangwar <himanshugangwar@Himanshus-MacBook-Air.local>
2024-05-14 07:42:11 +0530

    Initial Commit

---
+++ src/main/java/com/gangwar/dbencryptionutils/DbencryptionutilsApplication.java
@@ -0,0 +1,13 @@
+package com.gangwar.dbencryptionutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbencryptionutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbencryptionutilsApplication.class, args);
+	}
+
+}
